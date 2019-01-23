3c65c5c1e740c1488bb2257c45b97aa7e94a07cd
Kevin Law <kevin@Thaumaturge-Pro.local>
2019-01-23 02:53:44 -0800

    First commit

---
+++ form.php
@@ -0,0 +1,10 @@
+<?php
+
+echo "<p>Welcome</p>" ; $_GET["name"];
+
+echo "<p>Your Email Address is</p>" ; $_GET["email"];
+
+echo "<p> Your number is</p>" ; $_GET["phone"];
+
+echo "<p> You are from </p>" ; $_GET["state"];
+?>
